cc830804877e3b7e2dc325aca82edb038e2bfdea
민기 <che08180@naver.com>
2024-06-06 16:08:22 +0900

    feat: Add Layout, Header, Fotter Components

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h2>UTF-CKP팀의 사이트 입니다.</h2>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h2>COUrse</h2>
+    </header>
+  );
+};
+
+export default Header;
