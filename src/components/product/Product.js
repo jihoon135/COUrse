60528470ed6114e2c9ce5d34fb59bce145d694ac
jihoon kwak <kwakjihoon1001@gmail.com>
2024-07-02 03:18:18 +0900

    feat: router 처리, product 컴포넌트 요구 사항 정리

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,3 +1,11 @@
 export default function Product() {
-    
+    return(
+    <div className="main">
+        <div>상자1</div> {/*이 상자가 검색 되는 물품 개수에 따라서 나와야 함*/}
+        {/*일단 검색 했을 때의 정보를 usestate로 바꿀 수 있어야 할 듯?*/}
+        {/*상자 안에는 상품에 대한 정보(상품명, 로켓 배송 여부, 별점, 판매 가격, 할인율 등)에 대한 value 필요 */}
+        
+    </div>  
+    )
+
 }
